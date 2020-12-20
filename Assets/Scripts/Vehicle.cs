cf60072867634379c7b9ea4db6c57d509a26145b
Xwilarg <xwilarg@yahoo.fr>
2020-12-20 19:34:25 +0100

    Improve cars to avoid collisions

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -7,8 +7,12 @@ public class Vehicle : MonoBehaviour
     private Node _objective;
     private Rigidbody _rb;
 
-    private const float _speed = 10f;
+    private const float _speed = 15f;
+    private const float _acceleration = .05f;
     private const float _torque = 0.1f;
+    private const float _vision = 20f;
+
+    private readonly float[] _rangeCheck = new[] { -.9f, -.6f, -.3f, 0f, .3f, .6f, .9f };
 
     private void Start()
     {
@@ -23,14 +27,36 @@ public class Vehicle : MonoBehaviour
         if (_objective == null) // No objective
             return;
 
-        _rb.velocity = transform.forward * _speed; 
+        var objVelocity = transform.forward * _speed; 
         var rot = Quaternion.LookRotation(_objective.transform.position - transform.position);
         rot = Quaternion.Slerp(transform.rotation, rot, _torque);
         _rb.MoveRotation(rot);
 
-        if (Vector3.Distance(_objective.transform.position, transform.position) < 1f)
-        {
+        if (Vector3.Distance(_objective.transform.position, transform.position) < 1f) // Setting next objective is we reached the current one
             _objective = _objective.NextNodes[Random.Range(0, _objective.NextNodes.Length)];
+
+        foreach (var r in _rangeCheck) // Object detection
+        {
+            if (Physics.Raycast(transform.position + transform.forward * 1.1f, transform.forward * 3f + transform.right * r, out RaycastHit hit, 10f))
+            {
+                var val = hit.distance * _speed / _vision; // Slow down depending of obstacle distance
+                var mult = (val - 1f) / _speed;
+                if (mult < 0f) mult = 0f;
+                objVelocity *= mult;
+                break;
+            }
+        }
+        var currSpeed = Mathf.Lerp(_rb.velocity.magnitude, objVelocity.magnitude, _acceleration);
+        _rb.velocity = objVelocity.normalized * currSpeed;
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        foreach (var r in _rangeCheck)
+        {
+            Gizmos.DrawLine(transform.position + transform.forward * 1.1f, transform.position + (transform.forward * 3f + transform.right * r).normalized * _vision);
         }
     }
 }
