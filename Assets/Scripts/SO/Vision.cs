32938a583835fe4c82c538870ee008cf42640c33
Xwilarg <xwilarg@yahoo.fr>
2021-02-26 16:07:32 +0100

    Fix missing scripts

---
+++ Assets/Scripts/SO/Vision.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrafficSimulator.SO
+{
+    [Serializable]
+    public class Vision
+    {
+        public float AngleBase, AngleStep;
+        public float OffsetBase, OffsetStep;
+        public float Size;
+    }
+}
